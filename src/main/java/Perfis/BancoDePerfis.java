09c954164ced79a449f6198737aa8f5bb926b279
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2021-03-09 17:31:26 -0300

    Edita classes do Banco de Dados

--- src/main/java/Perfis/BancoDePerfis.java
+++ src/main/java/Perfis/BancoDePerfis.java
@@ -5,10 +5,65 @@
  */
 package Perfis;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author tpalvaro
  */
 public class BancoDePerfis {
-    
+
+    private ArrayList<Perfil> listaPerfis;
+    private Gson gson;
+
+    // converte objetos Java para JSON
+    private String json;
+
+    /**
+     *
+     * @param p - Lista de perfis
+     */
+    public BancoDePerfis(ArrayList<Perfil> p) {
+        listaPerfis = new ArrayList<>();
+        listaPerfis = p;
+        gson = new Gson();
+
+        json = gson.toJson(listaPerfis);
+    }
+
+    /**
+     * @throws IOException - precaução para casos de erros de leitura de
+     * arquivos
+     */
+    public void escreveArquivo() throws IOException {
+        try {
+            FileWriter writer = new FileWriter("perfis.json");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @return tabela - retona os perfis que foram traduzidos para Java
+     * @throws IOException - precaução para casos de erros de leitura de
+     * arquivos
+     */
+    public ArrayList pegaArquivo() throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("perfis.json"));
+
+            listaPerfis = gson.fromJson(br, ArrayList.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaPerfis;
+    }
 }
--- src/main/java/Programa/BancoDeSeries.java
+++ src/main/java/Programa/BancoDeSeries.java
@@ -5,10 +5,65 @@
  */
 package Programa;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author tpalvaro
  */
 public class BancoDeSeries {
-    
+
+    private ArrayList<Serie> listaSeries;
+    private Gson gson;
+
+    // converte objetos Java para JSON
+    private String json;
+
+    /**
+     *
+     * @param s - Lista de series
+     */
+    public BancoDeSeries(ArrayList<Serie> s) {
+        listaSeries = new ArrayList<>();
+        listaSeries = s;
+        gson = new Gson();
+
+        json = gson.toJson(listaSeries);
+    }
+
+    /**
+     * @throws IOException - precaução para casos de erros de leitura de
+     * arquivos
+     */
+    public void escreveArquivo() throws IOException {
+        try {
+            FileWriter writer = new FileWriter("series.json");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @return tabela - retona as series que foram traduzidas para Java
+     * @throws IOException - precaução para casos de erros de leitura de
+     * arquivos
+     */
+    public ArrayList pegaArquivo() throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("series.json"));
+
+            listaSeries = gson.fromJson(br, ArrayList.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaSeries;
+    }
 }
