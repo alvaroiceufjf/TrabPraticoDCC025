3f1d388fb426abcbabab75845c932d75a5bffc3e
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2021-03-09 17:00:49 -0300

    Adiciona Atributos em ConjuntoPerfis

--- src/main/java/Perfis/ConjuntoPerfis.java
+++ src/main/java/Perfis/ConjuntoPerfis.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Álvaro Domingues de Freitas     Matrícula: 201876007
  */
 package Perfis;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author tpalvaro
  */
 public class ConjuntoPerfis {
-    
+    private ArrayList <Perfil> perfis;
 }
